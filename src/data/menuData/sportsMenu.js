c70b996dc8fa982527102a190c5f92c8381968c4
DanBlakeNZ <dblakenz@hotmail.com>
2017-06-08 14:56:04 +1200

    sports added to mobile menu. mobile menu complete

--- src/data/menuData/sportsMenu.js
+++ src/data/menuData/sportsMenu.js
@@ -2,32 +2,38 @@ const sportsMenu = [
   {
     id: 1,
     catagory: 'Running',
-    imageUrl: 'https://res.cloudinary.com/dblakenzcloud/image/upload/v1496811431/Salomon/running.jpg'
+    imageUrl: 'https://res.cloudinary.com/dblakenzcloud/image/upload/v1496811431/Salomon/running.jpg',
+    menuTextColor: 'rgba(255, 255, 255, 0.7)'
   },
   {
     id: 2,
-    catagory: 'Freeride and Touring',
-    imageUrl: 'https://res.cloudinary.com/dblakenzcloud/image/upload/v1496811551/Salomon/freeride.jpg'
+    catagory: 'Freeride & Touring',
+    imageUrl: 'https://res.cloudinary.com/dblakenzcloud/image/upload/v1496811551/Salomon/freeride.jpg',
+    menuTextColor: 'rgba(0, 0, 14, 0.7)'
   },
   {
     id: 3,
     catagory: 'Nordic',
-    imageUrl: 'https://res.cloudinary.com/dblakenzcloud/image/upload/v1496811663/Salomon/nordic.jpg'
+    imageUrl: 'https://res.cloudinary.com/dblakenzcloud/image/upload/v1496811663/Salomon/nordic.jpg',
+    menuTextColor: 'rgba(0, 0, 14, 0.7)'
   },
   {
     id: 4,
     catagory: 'Hiking & Mountineering',
-    imageUrl: 'https://res.cloudinary.com/dblakenzcloud/image/upload/v1496811720/Salomon/hiking.jpg'
+    imageUrl: 'https://res.cloudinary.com/dblakenzcloud/image/upload/v1496811720/Salomon/hiking.jpg',
+    menuTextColor: 'rgba(255, 255, 255, 0.7)'
   },
   {
     id: 5,
     catagory: 'Alpine Racing',
-    imageUrl: 'https://res.cloudinary.com/dblakenzcloud/image/upload/v1496811803/Salomon/alpine.jpg'
+    imageUrl: 'https://res.cloudinary.com/dblakenzcloud/image/upload/v1496811803/Salomon/alpine.jpg',
+    menuTextColor: 'rgba(0, 0, 14, 0.7)'
   },
   {
     id: 6,
     catagory: 'Snowboarding',
-    imageUrl: 'https://res.cloudinary.com/dblakenzcloud/image/upload/v1496811865/Salomon/snowboard.jpg'
+    imageUrl: 'https://res.cloudinary.com/dblakenzcloud/image/upload/v1496811865/Salomon/snowboard.jpg',
+    menuTextColor: 'rgba(0, 0, 14, 0.7)'
   }
 ]
 export default sportsMenu
