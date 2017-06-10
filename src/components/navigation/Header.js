5b468604056694079adf3778c92d9605fdcb9846
Daniel Blake <danielblake@Daniels-MacBook-Air.local>
2017-06-10 16:25:48 +1200

    start of desktop navigation menu

--- src/components/navigation/Header.js
+++ src/components/navigation/Header.js
@@ -11,20 +11,28 @@ const Header = ({props}) => {
 
   return (
     <header className='header' id='salomonHeaderWrapper'>
+      <div className='subheader desktop'>
+        <a className='locateStoreWrapper'>
+          <i className="fa fa-map-marker" aria-hidden="true"/>
+          <span>Store</span>
+        </a>
+      </div>
       <nav>
         <div className='navWrapper'>
-          <div id='burger' onClick={mobileMenuToggle}>
+          <div id='burger' className='mobile' onClick={mobileMenuToggle}>
             <div />
             <div />
             <div />
           </div>
-          <a href='http://www.salomon.com/nz/' className='logo'>
+          <a href='http://www.salomon.com/nz/' className='logo-mobile'>
             <div>
-              <img className='mobile mobile_logo' alt='salomon' src='https://res.cloudinary.com/dblakenzcloud/image/upload/v1496635759/Salomon/logo-salomon-mobile.svg' />
+              <img className='mobile_logo mobile' alt='salomonLogo' src='https://res.cloudinary.com/dblakenzcloud/image/upload/v1496635759/Salomon/logo-salomon-mobile.svg' />
+              <img className='desktop_logo desktop' alt='salomonLogo'src='https://res.cloudinary.com/dblakenzcloud/image/upload/v1497068581/Salomon/logo-salomon-desktop.svg' />
+
             </div>
           </a>
           <div className='cart_wrapper'>
-            <i className='fa fa-shopping-bag' aria-hidden='true' />
+            <i className='fa fa-shopping-bag mobile' aria-hidden='true' />
           </div>
         </div>
       </nav>
