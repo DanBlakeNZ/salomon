45725030417dafc2fa4716a7b4f2af3e624e8acc
DanBlakeNZ <dblakenz@hotmail.comm>
2017-06-15 13:11:41 +1200

    working desktop slider and thumbnails

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,41 +1,16 @@
 import React from 'react'
-import CarouselItem from './carousel/CarouselItem'
-import carouselData from '../../data/carouselData/carouselData'
-
-import $ from 'jquery';
-import 'owl.carousel'
+import Carousel from './carousel/Carousel'
+import carouselFunctions from './carousel/functions/carouselFunctions'
 
 class HomePage extends React.Component {
   render () {
-    $(document).ready(function(){
-      let owl = $('.owl-carousel');
-
-      owl.owlCarousel({
-        items:1,
-        loop:true,
-        autoplay:true,
-        autoplayTimeout:5000,
-        autoplayHoverPause:true
-      });
-      $('.play').on('click',function(){
-        owl.trigger('play.owl.autoplay',[1000])
-      })
-      $('.stop').on('click',function(){
-        owl.trigger('stop.owl.autoplay')
-      })
-    })
-
+    carouselFunctions() // Calls all related carouselFunctions
     return (
       <div id='homepage-wrapper'>
-          <div id='carousel-wrapper'>
-            <div className="owl-carousel owl-theme">
-              {carouselData.map((carouselItemData) => {
-                return (
-                  <CarouselItem carouselItemData={carouselItemData} key={carouselItemData.id} />
-                )
-              })}
-          </div>
-          </div>
+        <section id='carousel-section'>
+          <Carousel />
+        </section>
+
       </div>
     )
   }
