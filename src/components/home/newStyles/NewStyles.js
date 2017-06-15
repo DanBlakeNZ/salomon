1f4114993055d505980fab43a02dee3b6689973d
DanBlakeNZ <dblakenz@hotmail.comm>
2017-06-15 15:13:13 +1200

    set up initial header stylings

---
+++ src/components/home/newStyles/NewStyles.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NewStyles = () => {
+  return (
+    <div id='newStylesWrapper'>
+      <h2 className='homepageSectionHeader'>New Styles</h2>
+    </div>
+  )
+}
+
+
+export default NewStyles
