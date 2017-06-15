6d1e7970c1cd86d2ba07b695c6e16cfa0660988b
DanBlakeNZ <dblakenz@hotmail.comm>
2017-06-15 15:37:14 +1200

    created new styles components and render image and product name

--- src/components/home/newStyles/NewStyles.js
+++ src/components/home/newStyles/NewStyles.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import NewStylesItem from './NewStylesItem'
+
+import newStylesData from '../../../data/newStylesData/newStylesData'
 
 const NewStyles = () => {
   return (
     <div id='newStylesWrapper'>
       <h2 className='homepageSectionHeader'>New Styles</h2>
+        {newStylesData.map((itemData) => {
+          return (
+            <NewStylesItem itemData={itemData} key={itemData.id} />
+          )
+        })}
     </div>
   )
 }
