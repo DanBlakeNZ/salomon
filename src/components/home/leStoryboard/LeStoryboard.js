ad2e7ec873dcb82b06205c5f73c0cc322b0ff34d
DanBlakeNZ <dblakenz@hotmail.comm>
2017-06-16 16:39:54 +1200

    added images and text for mobile

--- src/components/home/leStoryboard/LeStoryboard.js
+++ src/components/home/leStoryboard/LeStoryboard.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import LeStoryboardItem from './LeStoryboardItem'
+
+import leStoryboardData from '../../../data/leStoryboardData/leStoryboardData.js'
 
 const LeStoryBoard = () => {
   return (
@@ -6,7 +9,19 @@ const LeStoryBoard = () => {
       <h2 className='homepageSectionHeader'>Le Storyboard</h2>
       <p className='homepageSectionSubtitle'>Stories from Within our Walls and Beyond</p>
       <div className='storyboardTilesContainer'>
-
+        {leStoryboardData.map((leStoryboardItemData) => {
+          return (
+            <LeStoryboardItem leStoryboardItemData={leStoryboardItemData} key={leStoryboardItemData.id} />
+          )
+        })}
+        <div className='storyboardItem viewAll'>
+          <div className='storyboardContentWrapper'>
+            <p className='viewAllText'>View All Content</p>
+              <span className='desktopMenuArrowButton'>
+                <i className="fa fa-long-arrow-right" aria-hidden="true" />
+              </span>
+          </div>
+        </div>
       </div>
     </div>
   )
---
+++ src/components/home/leStoryboard/LeStoryboardItem.js
@@ -0,0 +1,29 @@
+import React, {PropTypes} from 'react'
+
+const LeStoryBoardItem = ({leStoryboardItemData}) => {
+
+  let styles = {
+    backgroundImage: 'url(' + leStoryboardItemData.imageUrl + ')'
+  }
+
+  return (
+    <div className='storyboardItem' style={styles}>
+      <div className='storyboardContentWrapper'>
+        <p className='storyboardDate'>
+          {leStoryboardItemData.date}
+        </p>
+        <p className='storyboardTitle'>
+          {leStoryboardItemData.articleTitle}
+        </p>
+
+      </div>
+    </div>
+  )
+}
+
+LeStoryBoardItem.propTypes = {
+  leStoryboardItemData: PropTypes.object.isRequired
+}
+
+
+export default LeStoryBoardItem
