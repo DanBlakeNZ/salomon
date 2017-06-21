d370e4840226a18cd3dd88089477f86865bb5af4
DanBlakeNZ <dblakenz@hotmail.comm>
2017-06-22 10:37:04 +1200

    added text to footer sections

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,17 +1,74 @@
 import React from 'react'
+import Email from './Email'
 
 const Footer = () => {
   return (
     <footer>
       <div className='footerWrapper'>
         <div className='footerRow'>
-          <div className='emailContainer'>
-            <p>Be A Salomon Insider! Subscribe to the Newsletter!</p>
-            <form className='emailForm'>
-              <input id='emailInput' type='text' placeholder='Your e-mail'></input>
-              <button id='emailButton'><i className="fa fa-long-arrow-right" aria-hidden="true"/></button>
-            </form>
+          <Email />
+        </div>
+        <div className='footerRow'>
+          <div className='footerCol'>
+            <h3>Stay Connected</h3>
+            <ul>
+              <li className='footerSocial'>
+                <i className="fa fa-facebook" aria-hidden="true" />
+              </li>
+              <li className='footerSocial'>
+                <i className="fa fa-twitter" aria-hidden="true" />
+                </li>
+              <li className='footerSocial'>
+                <i className="fa fa-instagram" aria-hidden="true" />
+              </li>
+              <li className='footerSocial'>
+                <i className="fa fa-youtube-play" aria-hidden="true" />
+              </li>
+            </ul>
           </div>
+
+          <div className='footerCol'>
+            <h3>Change Your Country</h3>
+          </div>
+
+          <div className='footerCol'>
+            <h3>About Salomon</h3>
+            <ul>
+              <li>Press Center</li>
+              <li>Careers</li>
+              <li>Find your dealer</li>
+              <li>Subscribe to newsletter</li>
+            </ul>
+          </div>
+
+          <div className='footerCol'>
+            <h3>Need Help</h3>
+          </div>
+
+          <div className='footerCol'>
+            <h3>Access</h3>
+            <ul>
+              <li>Login as VIP</li>
+              <li>Login as PRO</li>
+              <li>Salomon TV</li>
+            </ul>
+          </div>
+
+          <div className='footerCol'>
+            <h3>Collection</h3>
+            <ul>
+              <li>Men / S/LAB Collection</li>
+              <li>Men / Forces</li>
+              <li>Men / Black Edition</li>
+              <li>Men / Speedcross series</li>
+              <li>Women / S/LAB Collection</li>
+              <li>Women / Black Edition</li>
+              <li>Women / Speedcross series</li>
+              <li>kids / S/LAB Collection</li>
+              <li>kids / Speedcross series</li>
+            </ul>
+          </div>
+
         </div>
       </div>
     </footer>
